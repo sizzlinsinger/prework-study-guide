1235eda168d64c4f1a66b93106d590378ea7064d
sizzlinsinger <clsinger7@gmail.com>
2024-11-03 17:33:39 -0700

    Added JavaScript to project

--- assets/script.js
+++ assets/script.js
@@ -1,19 +1,32 @@
 const topics = ['HTML', 'CSS', 'Git', 'JavaScript'];
 
-for(let x = 0; x < topics.length; x++) {
+const randomTopic = topics[Math.floor(Math.random() * topics.length)];
+
+function listTopics() {
+    for (let x = 0; x < topics.length; x++) {
     console.log(topics[x]);
+    }
 }
 
-/*
-if (topic === 'HTML') {
+function selectTopics() {
+
+    if (randomTopic === 'HTML') {
   console.log("Let's study HTML!");
-} else if (topic === 'CSS') {
+    } else if (randomTopic === 'CSS') {
   console.log("Let's study CSS!");
-} else if (topic === 'Git') {
+    } else if (randomTopic === 'Git') {
   console.log("Let's study Git!");
-} else if (topic === 'JavaScript') {
+    } else if (randomTopic === 'JavaScript') {
   console.log("Let's study JavaScript!");
-} else {
+    } else {
   console.log('Please try again!');
+    }
 }
-*/
+
+console.log('Here are the topics we learned through the Prework:');
+
+listTopics()
+
+console.log('Which topic should we study first?');
+
+selectTopics()
